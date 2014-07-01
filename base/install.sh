4b2af9c89aa0c48d69088694825dbf89aa7dfe8c
Peter Jenkins <peter@funroll.co>
2014-07-01 14:33:07 -0400

    Update paths, source bashrc

--- base/install.sh
+++ base/install.sh
@@ -1,3 +1,4 @@
+source $PUBLIC_DIR/bashrc
 BASE_DIR=$PUBLIC_DIR/base
-ln -s $BASE_DIR/bashrc .bashrc
-ln -s $BASE_DIR/bash_profile .bash_profile
+ln -s $BASE_DIR/bashrc $HOME/.bashrc
+ln -s $BASE_DIR/bash_profile $HOME/.bash_profile
